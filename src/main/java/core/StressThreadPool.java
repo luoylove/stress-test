3504578b3f589d889cb9560ec9ad99566ff4f9db
luoyoujun <luoyoujun3712@dingtalk.com>
2020-06-18 13:34:03 +0800

    update threadPool name

--- src/main/java/core/StressThreadPool.java
+++ src/main/java/core/StressThreadPool.java
@@ -7,8 +7,10 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 线程池工具
@@ -98,8 +100,10 @@ public class StressThreadPool {
          */
         final RejectedExecutionHandler handler = new ThreadPoolExecutor.AbortPolicy();
 
+        final ThreadFactory threadFactory = new StressTestThreadFactory();
+
         threadPool = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS,
-                workQueue, handler);
+                workQueue, threadFactory, handler);
 
         return this;
     }
@@ -137,5 +141,35 @@ public class StressThreadPool {
      */
     public boolean isTerminated(){
         return threadPool.isTerminated();
+    }
+
+    static class StressTestThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        StressTestThreadFactory() {
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() :
+                    Thread.currentThread().getThreadGroup();
+            namePrefix = "stressWorker-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon()) {
+                t.setDaemon(false);
+            }
+            if (t.getPriority() != Thread.NORM_PRIORITY) {
+                t.setPriority(Thread.NORM_PRIORITY);
+            }
+            return t;
+        }
     }
 }
