65caddfd28859afece2cfe8608dd1be787aac6b1
luoyoujun <luoyoujun3712@dingtalk.com>
2020-07-08 10:56:19 +0800

    泛型bug

--- src/main/java/com/ly/core/taskimpl/AbstractHttpTask.java
+++ src/main/java/com/ly/core/taskimpl/AbstractHttpTask.java
@@ -1,12 +1,13 @@
 package com.ly.core.taskimpl;
 
 import com.ly.core.StressTask;
+import com.ly.core.util.HttpResponse;
 
 /**
  * @Author: luoy
  * @Date: 2020/6/19 13:43.
  */
-public abstract class AbstractHttpTask<HttpResponse> implements StressTask {
+public abstract class AbstractHttpTask implements StressTask<HttpResponse> {
     @Override
     public HttpResponse task() throws Throwable {
         return doTask();
